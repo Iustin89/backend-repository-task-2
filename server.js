5450bb5ea7b3632eae88ee4b7d443d85e9e4f83e
justin <damii_justin@yahoo.com>
2016-03-17 16:41:38 +0200

    create var!

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 
 // cream variabilele si apelam pachetele necesare aplicatiei
 var http = require('http');
-var express = require('express');                          
-var app = express();                                       
+var express = require('express');
+var exphbs = require('express-handlebars');
+var moment = require('moment');
 var bodyParser = require('body-parser');
-
 var api = require('instagram-node').instagram();
+var app = express();
+
 Instagram = require('instagram-node-lib');
 
 Instagram.set('client_id', 'caf0205ad64b4ab5a796e782a48f8a89');
@@ -21,6 +23,10 @@ Instagram.set('redirect_uri', 'https://twitter.com/?lang=en');
 // CONFIGURARI NECESARE LUCRULUI CU bodyParser() - ne va permite sa returnam data dintr-un POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
 
 app.set('port', process.env.PORT || 3030);                  // setam un port definit de noi local 
